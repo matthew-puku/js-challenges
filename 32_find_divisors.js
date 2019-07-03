a15f55833a52ce6431fb7c9752e0608e1e48841d
Matthew Puku <matthew.puku@gmail.com>
2019-07-04 09:45:14 +1000

    Solved 32_find_divisors

--- 32_find_divisors.js
+++ 32_find_divisors.js
@@ -9,3 +9,24 @@ divisors(25); //should return [5]
 divisors(13); //should return "13 is prime"
 You can assume that you will only get positive integers as inputs.
 */
+
+const divisors = (n) => {
+  const largestPossibleDivisor = n / 2
+  let divisors = []
+  
+  let i = 2
+  while (i <= largestPossibleDivisor) {
+    if (n % i === 0) {
+      divisors.push(i)
+    }
+    i++
+  }
+
+  if (divisors.length) {
+    return divisors
+  } else {
+    return `${n} is prime`
+  }
+}
+
+console.log(divisors(12))
